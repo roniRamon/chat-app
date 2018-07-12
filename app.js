9f1c32a7aee0aa02c72d24fe0e56271682ceda01
RoniRamon <roniramon260@gmail.com>
2018-07-11 17:39:27 -0700

    server part 2

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const http = require('http').Server(app);
 const path = require('path');
 
-// const chatServer = require('./lib/chatServer');
-// chatServer.listen(http);
+const chatServer = require('./lib/chatServer');
+chatServer.listen(http);
 
 const PORT = 8000;
 
